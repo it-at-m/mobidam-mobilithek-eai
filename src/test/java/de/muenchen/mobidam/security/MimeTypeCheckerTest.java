83c31a8a12cd42bd7dd4cdf3a5413bcaea8fc51b
martin.dietrich <martin.dietrich@muenchen.de>
2024-09-06 07:46:51 +0200

    unit-test added

--- src/test/java/de/muenchen/mobidam/security/MimeTypeCheckerTest.java
+++ src/test/java/de/muenchen/mobidam/security/MimeTypeCheckerTest.java
@@ -9,21 +9,29 @@ import java.util.List;
 
 public class MimeTypeCheckerTest {
 
+    private static final String TEXT_CONTENT = "test content with plain text";
+    private static final String XML_CONTENT = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root>test content</root>";
+
     private final MimeTypeChecker mimeTypeChecker = new MimeTypeChecker();
 
     @Test
     public void testCheckWithValidPlainText() throws IOException {
-        Assertions.assertTrue(mimeTypeChecker.check(new ByteArrayInputStream("test content with plain text".getBytes()), List.of("text/plain")));
+        Assertions.assertTrue(mimeTypeChecker.check(new ByteArrayInputStream(TEXT_CONTENT.getBytes()), List.of("text/plain")));
     }
 
     @Test
     public void testCheckWithValidXml() throws IOException {
-        Assertions.assertTrue(mimeTypeChecker.check(new ByteArrayInputStream("<?xml version=\"1.0\" encoding=\"UTF-8\"?><root>test content</root".getBytes()),
+        Assertions.assertTrue(mimeTypeChecker.check(new ByteArrayInputStream(XML_CONTENT.getBytes()),
                 List.of("application/xml")));
     }
 
     @Test
     public void testCheckWithInvalidMimeType() throws IOException {
-        Assertions.assertFalse(mimeTypeChecker.check(new ByteArrayInputStream("test content with plain text".getBytes()), List.of("application/xml")));
+        Assertions.assertFalse(mimeTypeChecker.check(new ByteArrayInputStream(TEXT_CONTENT.getBytes()), List.of("application/xml")));
+    }
+
+    @Test
+    public void testCheckWithInvalidMimeType2() throws IOException {
+        Assertions.assertFalse(mimeTypeChecker.check(new ByteArrayInputStream(XML_CONTENT.getBytes()), List.of("text/plain")));
     }
 }
